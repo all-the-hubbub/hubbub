47b2a76c8454edcac7e4dfb433c81ade38c5b13a
Sarah Allen <sarahallen@google.com>
2017-04-19 13:41:30 -0700

    add material with button

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs';      // for Material gestures
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,  // used by Material components
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,
+    MdCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
