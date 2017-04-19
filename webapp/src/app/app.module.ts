a7a0d8b987fceb38c7f04dfc9ccd1cc58c1d7a08
Sarah Allen <sarahallen@google.com>
2017-04-19 15:20:21 -0700

    sets data in RTDB, basic auth

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { MdButtonModule, MdCheckboxModule } from '@angular/material';
@@ -10,11 +13,25 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';      // for Material gestures
 
+// For AoT compatibility, this needs to be exported so that Angular
+// can statically analyze the NgModule declaration
+export const config = {
+    apiKey: "AIzaSyC2nnXNrZafMKoAqrwU69AkSJP6iwqJFJ0",
+    authDomain: "hubbub-159904.firebaseapp.com",
+    databaseURL: "https://hubbub-159904.firebaseio.com",
+    projectId: "hubbub-159904",
+    storageBucket: "hubbub-159904.appspot.com",
+    messagingSenderId: "113654972557"
+  };
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
     BrowserAnimationsModule,  // used by Material components
     BrowserModule,
     FormsModule,
