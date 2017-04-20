b0f1e05c3f1590933307867cc847125a9f0499c5
Justin Rosenthal <rosenthalj@google.com>
2017-04-19 18:29:13 -0700

    Implement an HTTP function scaffold for building a profile from GitHub

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
-var functions = require('firebase-functions');
+const datastore = require('@google-cloud/datastore')();
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.buildProfile = functions.https.onRequest((req, res) => {
+  const uid = req.query.uid;
+  if (!uid) {
+    res.sendStatus(400);
+    return;
+  }
+
+  return admin.database().ref('/account/' + uid).once('value')
+    .then(snapshot => {
+      const account = snapshot.val();
+      if (!account || !account.token) {
+        res.sendStatus(401);
+        return;
+      }
+
+      // Talk to Github, do stuff...
+
+      // Save stuff to Datastore
+      const key = datastore.key(['User', uid]);
+      const user = {
+        key: key,
+        data: {
+          email: account.email,
+        },
+      };
+      return datastore.save(user);
+    })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
