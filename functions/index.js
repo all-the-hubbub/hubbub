cf4c112c1db3b2e0c4ee667e7a7fba258180201f
Justin Rosenthal <rosenthalj@google.com>
2017-05-01 12:24:41 -0700

    Temporary implementation of updateProfile function

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,27 @@ exports.closeSlot = functions.https.onRequest((req, res) => {
     });
 });
 
+// TODO: Temporary until a more formal, tested implementation is written
+const GitHubbub = require('./githubbub');
+exports.updateProfile = functions.database.ref("/accounts/{userId}/githubToken")
+  .onWrite(event => {
+    const token = event.data.val();
+    if (!token) {
+      console.log('Token is missing');
+      return;
+    }
+
+    const github = new GitHubbub(token);
+    const userId = event.params.userId;
+    const profileRef = event.data.ref.root.child(`/profiles/${userId}`);
+
+    return github.profile()
+      .then(data => {
+        return profileRef.update({
+          uid: userId,
+          name: data.name,
+          handle: data.login,
+          photo: data.avatar_url,
+        });
+      });
+  });
