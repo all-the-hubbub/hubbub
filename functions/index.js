a9466e6428a12a71ac0840338abf350a663223fd
Sarah Allen <sarahallen@google.com>
2017-05-01 16:44:55 -0700

    cors for slot functions needed by web client

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,18 @@ const slots = require('./slots');
 // allow access from anywhere, since all functions will require authentication
 const cors = require('cors')({origin: true});
 
-exports.joinSlot = functions.https.onRequest(slots.join);
-exports.leaveSlot = functions.https.onRequest(slots.leave);
+exports.joinSlot = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      slots.join(req, res);
+    });
+});
+
+exports.leaveSlot = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      slots.leave(req, res);
+    });
+});
+
 exports.closeSlot = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
       slots.close(req, res);
