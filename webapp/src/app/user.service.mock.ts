89a598c3738eaade8e180d62f62d9967338ceca2
Sarah Allen <sarahallen@google.com>
2017-05-17 14:39:35 -0700

    fix tests

--- webapp/src/app/user.service.mock.ts
+++ webapp/src/app/user.service.mock.ts
@@ -1,20 +1,29 @@
 import { Observable } from 'rxjs/Observable';
-import { Profile }  from './types'
+import { Profile, Account }  from './types'
 import { AuthStatus } from './user.service';
 
 
 export const subjectUid = '1234';
+export const subjectEmail = 'someone@whatever.com';
 export const subjectProfileData = {
     photo: 'https://whatever.com/photo.jpg',
-    email: 'someone@whatever.com',
+    email: subjectEmail,
     name: 'Maria Sanchez'
 };
+
+export const subjectAccountData = {
+    email: subjectEmail,
+    admin: false,
+    githubToken: '1234abcd'
+};
+
 
 // Mock for loggedIn user
 export class UserServiceMock {
   public loginStatus: AuthStatus = "LoggedIn";
   public profile$: Observable<Profile>;
   public user$: Observable<any>;
+  public account$: Observable<Account | undefined>;
 
   constructor() {
     let user = {
@@ -27,7 +36,9 @@ export class UserServiceMock {
     this.profile$ = Observable.create(function (observer) {
       observer.next(subjectProfileData);
     });
-
+    this.account$ = Observable.create(function (observer) {
+      observer.next(subjectAccountData);
+    });
   }
 
 }
